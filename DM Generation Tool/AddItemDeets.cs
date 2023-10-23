197fd073c87efd3963a4b5eacfec8c327514af5b
Anteny Erdman <anteny30@gmail.com>
2023-10-23 15:35:06 -0700

    Fixed a nesting issue

--- DM Generation Tool/AddItemDeets.cs
+++ DM Generation Tool/AddItemDeets.cs
@@ -19,66 +19,50 @@ namespace DM_Generation_Tool
 
         private void OK_Click(object sender, EventArgs e)
         {
-            if (NameText.Text != string.Empty)
-            {
-                if(DesText.Text != string.Empty)
-                {
-                    if(PriceText.Text != string.Empty)
-                    {
-                        if(ThemeText.Text != string.Empty)
-                        {
-                            if(Type1Text.Text != string.Empty)
-                            {
-                                if (Type2Text.Text != string.Empty)
-                                {
-                                    if (Type3Text.Text != string.Empty)
-                                    {
-                                        if (Type4Text.Text != string.Empty)
-                                        {
-                                            int id = Databases.GetAmount("Shop") + 1;
-                                            Databases.AddShopItem(id, NameText.Text, DesText.Text, PriceText.Text,
-                                                ThemeText.Text, Type1Text.Text, Type2Text.Text, Type3Text.Text, Type4Text.Text); 
-                                            Close();
-                                        }
-                                        else
-                                        {
-                                            MessageBox.Show("Item type needs to be entered. None is acceptable");
-                                        }
-                                    }
-                                    else
-                                    {
-                                        MessageBox.Show("shop type 3 needs to be entered. None is acceptable");
-                                    }
-                                }
-                                else
-                                {
-                                    MessageBox.Show("shop type 2 needs to be entered. None is acceptable");
-                                }
-                            }
-                            else
-                            {
-                                MessageBox.Show("shop type 1 needs to be entered. None is acceptable");
-                            }
-                        }
-                        else
-                        {
-                            MessageBox.Show("Theme needs to be entered");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Price needs to be entered");
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Description needs to be entered");
-                }
-            }
-            else
+            if (NameText.Text == string.Empty)
             {
                 MessageBox.Show("Name needs to be entered");
+                return;
             }
+            if (DesText.Text != string.Empty)
+            {
+                MessageBox.Show("Description needs to be entered");
+                return;
+            }
+            if (PriceText.Text != string.Empty)
+            {
+                MessageBox.Show("Price needs to be entered");
+                return;
+            }
+            if (ThemeText.Text != string.Empty)
+            {
+                MessageBox.Show("Theme needs to be entered");
+                return;
+            }
+            if (Type1Text.Text != string.Empty)
+            {
+                MessageBox.Show("shop type 1 needs to be entered. None is acceptable");
+                return;
+            }
+            if (Type2Text.Text == string.Empty)
+            {
+                MessageBox.Show("shop type 2 needs to be entered. None is acceptable");
+                return;
+            }
+            if (Type3Text.Text == string.Empty)
+            {
+                MessageBox.Show("shop type 3 needs to be entered. None is acceptable");
+                return;
+            }
+            if (Type4Text.Text == string.Empty)
+            {
+                MessageBox.Show("Item type needs to be entered. None is acceptable");
+                return;
+            }
+            int id = Databases.GetAmount("Shop") + 1;
+            Databases.AddShopItem(id, NameText.Text, DesText.Text, PriceText.Text,
+                ThemeText.Text, Type1Text.Text, Type2Text.Text, Type3Text.Text, Type4Text.Text);
+            Close();
         }
     }
 }
